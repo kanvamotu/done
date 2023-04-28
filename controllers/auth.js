2d0c3dbce82735fae4b000becfe9ab98cb31b4a2
kanvamotu <pranjalsingh2609@gmail.com>
2023-04-28 01:31:57 -0700

    testing

--- controllers/auth.js
+++ controllers/auth.js
@@ -28,7 +28,7 @@ exports.signup = (req, res) => {
     user.save((err, user) => {
    if(err){
     return res.status(400).json({
-        err: "NOT able to save user in DB"
+        err: "NOT able to save user in DB "
     })
    }
    res.json(user)
